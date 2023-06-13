e077766bf981278b93a19449bbf6d12834d34934
Dockie-dev <agbaidorcas23@gmail.com>
2023-06-13 15:18:48 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,3 +15,15 @@ void print_binary(unsigned long int n)
 		printf("0");
 		return;
 	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
