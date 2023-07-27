6b4dd509a5bd7507a9954557224d43ecee3abc27
Dockie-dev <agbaidorcas@gmail.com>
2023-07-27 15:28:19 -0700

    2files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,13 +4,13 @@
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letters: Number of letters the
+ * @letters: The number of letters the
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - The actual number of bytes the function can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buffer;
