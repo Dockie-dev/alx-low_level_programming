0733c27bb25243fb1e47a5875162804fd8f5081f
Dockie-dev <agbaidorcas23@gmail.com>
2023-08-03 13:44:53 -0700

    All files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
